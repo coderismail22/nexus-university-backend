7645ca897c95145e4298ebf5a77939a1da4944c8
ismail <coderismail22@gmail.com>
2024-09-17 07:33:59 +0600

    add course and prerequisite course interface

---
+++ src/app/modules/Course/course.interface.ts
@@ -0,0 +1,14 @@
+import { Types } from "mongoose";
+
+export type TPrerequisiteCourse = {
+  course: Types.ObjectId; //actually referenced id
+  isDeleted: boolean;
+};
+
+export type TCourse = {
+  title: string;
+  prefix: string;
+  code: number;
+  credits: number;
+  preRequisiteCourses: [];
+}; 
