738c622ee1898e2828dd214addc62e6d6ab92192
ismail <coderismail22@gmail.com>
2024-09-18 16:28:59 +0600

    add semester registration boiler plate

---
+++ src/app/modules/semester-registration/semester-registration.service.ts
@@ -0,0 +1,18 @@
+// 1.Create semester registration
+const createSemesterRegistrationIntoDB = async () => {};
+
+// 2.Get all semester registrations
+const getAllSemesterRegistrationsFromDB = async () => {};
+
+// 3.Get a single semester registration
+const getSingleSemesterRegistrationFromDB = async () => {};
+
+// 4.Update semester registration
+const updateSemesterRegistrationIntoDB = async () => {};
+
+export const SemesterRegistrationServices = {
+  createSemesterRegistrationIntoDB,
+  getAllSemesterRegistrationsFromDB,
+  getSingleSemesterRegistrationFromDB,
+  updateSemesterRegistrationIntoDB,
+};
