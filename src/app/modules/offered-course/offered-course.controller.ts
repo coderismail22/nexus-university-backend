4ccef9eaad518f2b10cb25600f3428c7f01a652c
ismail <coderismail22@gmail.com>
2024-09-19 07:21:29 +0600

    add controller and service for creating offered course

--- src/app/modules/offered-course/offered-course.controller.ts
+++ src/app/modules/offered-course/offered-course.controller.ts
@@ -1,4 +1,19 @@
-const createOfferedCourse = async () => {};
+import httpStatus from "http-status";
+import sendResponse from "../../utils/sendResponse";
+import { OfferedCourseServices } from "./offered-course.service";
+import catchAsync from "../../utils/catchAsync";
+
+const createOfferedCourse = catchAsync(async (req, res) => {
+  const result = await OfferedCourseServices.createOfferedCourseIntoDB(
+    req.body,
+  );
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "Offered course created successfully",
+    data: result,
+  });
+});
 const getAllOfferedCourses = async () => {};
 const getSingleOfferedCourse = async () => {};
 const updateSingleOfferedCourse = async () => {};
--- src/app/modules/offered-course/offered-course.service.ts
+++ src/app/modules/offered-course/offered-course.service.ts
@@ -1,5 +1,11 @@
+import { TOfferedCourse } from "./offered-course.interface";
+import { OfferedCourse } from "./offered-course.model";
+
 // Create an offered course
-const createOfferedCourseIntoDB = async () => {};
+const createOfferedCourseIntoDB = async (payload: TOfferedCourse) => {
+  const result = await OfferedCourse.create(payload);
+  return result;
+};
 // Get all offered courses
 const getAllOfferedCoursesFromDB = async () => {};
 // Get a single offered course
