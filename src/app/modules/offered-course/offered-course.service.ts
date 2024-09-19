a2305ba1957ba36d5defa75623ea03a7969326fb
ismail <coderismail22@gmail.com>
2024-09-19 07:50:19 +0600

    add strict checking for creating offered courses

--- src/app/modules/offered-course/offered-course.service.ts
+++ src/app/modules/offered-course/offered-course.service.ts
@@ -1,9 +1,43 @@
+import httpStatus from "http-status";
+import AppError from "../../errors/AppError";
 import { TOfferedCourse } from "./offered-course.interface";
 import { OfferedCourse } from "./offered-course.model";
 
 // Create an offered course
 const createOfferedCourseIntoDB = async (payload: TOfferedCourse) => {
-  
+  const {
+    academicDepartment,
+    academicSemester,
+    academicFaculty,
+    course,
+    semesterRegistration,
+    faculty,
+  } = payload;
+
+  // 1.check department
+  if (!academicDepartment) {
+    throw new AppError(httpStatus.NOT_FOUND, "Academic department not found");
+  }
+  // 2.check semester
+  if (!academicSemester) {
+    throw new AppError(httpStatus.NOT_FOUND, "Academic semester not found");
+  }
+  // 3.check academic faculty
+  if (!academicFaculty) {
+    throw new AppError(httpStatus.NOT_FOUND, "Academic faculty not found");
+  }
+  // 4.check course
+  if (!course) {
+    throw new AppError(httpStatus.NOT_FOUND, "Course not found");
+  }
+  // 5.check registration
+  if (!semesterRegistration) {
+    throw new AppError(httpStatus.NOT_FOUND, "Semester registration not found");
+  }
+  // 6.check faculty
+  if (!faculty) {
+    throw new AppError(httpStatus.NOT_FOUND, "Faculty not found");
+  }
   const result = await OfferedCourse.create(payload);
   return result;
 };
