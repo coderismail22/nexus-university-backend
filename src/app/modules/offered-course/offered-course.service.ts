23b3139a450457400c803c064a6aa39ede18e955
ismail <coderismail22@gmail.com>
2024-09-19 07:07:13 +0600

    add boiler code for offered-course services

---
+++ src/app/modules/offered-course/offered-course.service.ts
@@ -0,0 +1,15 @@
+// Create an offered course
+const createOfferedCourseIntoDB = async () => {};
+// Get all offered courses
+const getAllOfferedCoursesFromDB = async () => {};
+// Get a single offered course
+const getSingleOfferedCourseFromDB = async () => {};
+// Update a single offered course
+const updateOfferedCourseIntoDB = async () => {};
+
+export const OfferedCourseServices = {
+  createOfferedCourseIntoDB,
+  getAllOfferedCoursesFromDB,
+  getSingleOfferedCourseFromDB,
+  updateOfferedCourseIntoDB,
+};
