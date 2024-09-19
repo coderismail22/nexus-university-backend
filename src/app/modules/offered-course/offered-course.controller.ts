253df185a251520ac17babf8730ccd269da2de40
ismail <coderismail22@gmail.com>
2024-09-19 07:09:37 +0600

    add boiler code for offered-course controllers

---
+++ src/app/modules/offered-course/offered-course.controller.ts
@@ -0,0 +1,11 @@
+const createOfferedCourse = async () => {};
+const getAllOfferedCourses = async () => {};
+const getSingleOfferedCourse = async () => {};
+const updateSingleOfferedCourse = async () => {};
+
+export const OfferedCourseControllers = {
+  createOfferedCourse,
+  getAllOfferedCourses,
+  getSingleOfferedCourse,
+  updateSingleOfferedCourse,
+};
