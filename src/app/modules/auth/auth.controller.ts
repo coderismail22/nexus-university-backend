dccb12a217094526c22a04418bd7f3c7ef44584e
ismail <coderismail22@gmail.com>
2024-09-21 16:25:29 +0600

    set refreshToken through cookie

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -1,19 +1,29 @@
 import httpStatus from "http-status";
 import sendResponse from "../../utils/sendResponse";
-import { User } from "../user/user.model";
+// import { User } from "../user/user.model";
 import { AuthServices } from "./auth.service";
 import catchAsync from "../../utils/catchAsync";
+import config from "../../config";
 
 // login controller
 const loginUser = catchAsync(async (req, res) => {
   const result = await AuthServices.loginUser(req.body);
+  const { accessToken, refreshToken, needsPasswordChange } = result;
+  res.cookie("refreshToken", refreshToken, {
+    secure: config.NODE_ENV === "production",
+    httpOnly: true,
+  });
+
   sendResponse(res, {
     statusCode: httpStatus.OK,
     success: true,
     message: "Login successful",
-    data: result,
+    data: {
+      accessToken,
+      needsPasswordChange,
+    },
   });
-}); 
+});
 
 // change password controller
 const changePassword = catchAsync(async (req, res) => {
