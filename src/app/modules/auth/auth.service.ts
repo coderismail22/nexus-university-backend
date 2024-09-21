ecab7624ba368a4bfd5e385542339e9d6f274ac7
ismail <coderismail22@gmail.com>
2024-09-21 09:35:05 +0600

    update  validateRequest

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -40,11 +40,7 @@ const loginUser = async (payload: TLoginUser) => {
   }
   //   TODO: send access and refresh token
 
-  // create token and send to the client
-  const jwtPayload = { userId: user?.id, role: user?.role };
-
-  console.log(config.jwt_access_secret as string)
-
+  // create token and send to the cl
   const accessToken = jwt.sign(jwtPayload, config.jwt_access_secret as string, {
     expiresIn: "10d",
   });
