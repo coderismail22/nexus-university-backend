20232285ada8ea14829c349860ca9b7137d6413b
ismail <coderismail22@gmail.com>
2024-09-22 09:41:28 +0600

    use cookie parser and set cors origins

--- src/app.ts
+++ src/app.ts
@@ -2,20 +2,22 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import cors from 'cors';
-import express, { Application, Request, Response } from 'express';
-import globalErrorHandler from './app/middlewares/globalErrorhandler';
-import notFound from './app/middlewares/notFound';
-import router from './app/routes';
+import cors from "cors";
+import express, { Application, Request, Response } from "express";
+import globalErrorHandler from "./app/middlewares/globalErrorhandler";
+import notFound from "./app/middlewares/notFound";
+import router from "./app/routes";
+import cookieParser from "cookie-parser";
 
 const app: Application = express();
 
 //parsers
 app.use(express.json());
-app.use(cors());
+app.use(cookieParser());
+app.use(cors({ origin: ["http://localhost:5000/api/v1"] }));
 
 // application routes
-app.use('/api/v1', router);
+app.use("/api/v1", router);
 
 const test = async (req: Request, res: Response) => {
   const a = 200;
