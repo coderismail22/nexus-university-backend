8e06086b57cb91660937d0ac8d8659e024f68952
ismail <coderismail22@gmail.com>
2024-10-18 11:56:52 +0600

    update course routes

--- src/app/modules/Course/course.route.ts
+++ src/app/modules/Course/course.route.ts
@@ -3,11 +3,14 @@ import validateRequest from "../../middlewares/validateRequest";
 import { CourseValidations } from "./course.validation";
 import { CourseControllers } from "./course.controller";
 import { CourseFaculty } from "./course.model";
+import { USER_ROLE } from "../user/user.constant";
+import auth from "../../middlewares/auth";
 const router = express.Router();
 
 // Create a course
 router.post(
   "/create-academic-course",
+  auth(USER_ROLE.superAdmin, USER_ROLE.admin),
   validateRequest(CourseValidations.createCourseValidationSchema),
   CourseControllers.createCourse,
 );
@@ -15,6 +18,7 @@ router.post(
 // Update a course
 router.patch(
   "/:id",
+  auth(USER_ROLE.superAdmin, USER_ROLE.admin),
   validateRequest(CourseValidations.updateCourseValidationSchema),
   CourseControllers.updateCourse,
 );
@@ -22,6 +26,8 @@ router.patch(
 // Assign Faculties With Course
 router.put(
   "/:courseId/assign-faculties",
+  auth(USER_ROLE.superAdmin, USER_ROLE.admin),
+
   validateRequest(CourseValidations.facultiesWithCourseValidationSchema),
   CourseControllers.assignFacultiesWithCourseIntoDB,
 );
@@ -29,17 +35,41 @@ router.put(
 // Discharge Faculties From Course
 router.delete(
   "/:courseId/remove-faculties",
+  auth(USER_ROLE.superAdmin, USER_ROLE.admin),
+
   validateRequest(CourseValidations.facultiesWithCourseValidationSchema),
   CourseControllers.removeFacultiesFromCourseFromDB,
 );
 
 //Get all courses
-router.get("/get-all-academic-courses", CourseControllers.getAllCourses);
+router.get(
+  "/get-all-academic-courses",
+  auth(
+    USER_ROLE.superAdmin,
+    USER_ROLE.admin,
+    USER_ROLE.faculty,
+    USER_ROLE.student,
+  ),
+  CourseControllers.getAllCourses,
+);
 
 // Single a single course
-router.get("/:id", CourseControllers.getSingleCourse);
+router.get(
+  "/:id",
+  auth(
+    USER_ROLE.superAdmin,
+    USER_ROLE.admin,
+    USER_ROLE.faculty,
+    USER_ROLE.student,
+  ),
+  CourseControllers.getSingleCourse,
+);
 
 // Delete a single course
-router.delete("/:id", CourseControllers.deleteCourse);
+router.delete(
+  "/:id",
+  auth(USER_ROLE.superAdmin, USER_ROLE.admin),
+  CourseControllers.deleteCourse,
+);
 
 export const AcademicCourseRoutes = router;
