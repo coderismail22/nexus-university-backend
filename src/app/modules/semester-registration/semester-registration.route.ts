20401ad582d882b43a486c862f08192f0e1fc8a9
ismail <coderismail22@gmail.com>
2024-10-18 15:17:17 +0600

    update academic semester and registration route

--- src/app/modules/semester-registration/semester-registration.route.ts
+++ src/app/modules/semester-registration/semester-registration.route.ts
@@ -2,12 +2,15 @@ import express from "express";
 import validateRequest from "../../middlewares/validateRequest";
 import { SemesterRegistrationControllers } from "./semester-registration.controller";
 import { SemesterRegistrationValidations } from "./semester-registration.validation";
+import auth from "../../middlewares/auth";
+import { USER_ROLE } from "../user/user.constant";
 
 const router = express.Router();
 
 // Register
 router.post(
   "/create-academic-registration",
+  auth(USER_ROLE.superAdmin, USER_ROLE.admin),
   validateRequest(
     SemesterRegistrationValidations.createSemesterRegistrationValidationSchema,
   ),
@@ -17,18 +20,31 @@ router.post(
 // Get All Registrations
 router.get(
   "/get-all-semester-registrations",
+  auth(
+    USER_ROLE.superAdmin,
+    USER_ROLE.admin,
+    USER_ROLE.faculty,
+    USER_ROLE.student,
+  ),
   SemesterRegistrationControllers.getAllSemesterRegistrations,
 );
 
 // Get A Single Registration
 router.get(
   "/:id",
+  auth(
+    USER_ROLE.superAdmin,
+    USER_ROLE.admin,
+    USER_ROLE.faculty,
+    USER_ROLE.student,
+  ),
   SemesterRegistrationControllers.getSingleSemesterRegistration,
 );
 
 // Update A Single Registration
 router.patch(
   "/:id",
+  auth(USER_ROLE.superAdmin, USER_ROLE.admin),
   validateRequest(
     SemesterRegistrationValidations.updateSemesterRegistrationValidationSchema,
   ),
@@ -38,6 +54,7 @@ router.patch(
 // Delete A Single Registration
 router.delete(
   "/:id",
+  auth(USER_ROLE.superAdmin, USER_ROLE.admin),
   SemesterRegistrationControllers.deleteSemesterRegistrationAndAllAssociatedOfferedCourses,
 );
 
